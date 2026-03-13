c40790ffc067354e11ae0b0f4038210eedce589d
egorkharaim <egor.kharaima@gmail.com>
2026-03-13 19:06:07 +0300

    removing the lines with todo

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -11,7 +11,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public User login(String login, String password) throws AuthenticationException {
 
-        // TODO: add corresponding log message about method login was called
+       
         logger.info("Loggin method was called");
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
