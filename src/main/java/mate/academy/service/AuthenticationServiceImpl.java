661217b527e8855313242a77da6e63a843e1f014
egorkharaim <egor.kharaima@gmail.com>
2026-03-14 01:15:03 +0300

    Added parameters in the log messages

--- src/main/java/mate/academy/service/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/service/AuthenticationServiceImpl.java
@@ -11,7 +11,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public User login(String login, String password) throws AuthenticationException {
 
-        logger.info("Loggin method was called");
+        logger.info("Usser is logged, login: {}", login);
         User user = findByLogin(login);
         if (!user.getPassword().equals(password)) {
             throw new AuthenticationException("Username or password are incorrect");
